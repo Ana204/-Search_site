3e545c9d366f097412e9177238cc7df836dbeb6f
nicolasmoura23 <thenicou@gmail.com>
2020-10-28 17:15:36 -0300

    Getting up the card class and your css

--- frontend/src/Layouts/Researchs/index.js
+++ frontend/src/Layouts/Researchs/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './styles.css';
+import ImagemWorld from './img/worldverde.jpg';
 
 const Researchs = () => {
     return (
         <div className="fundo-verde">
             <div className="container-branco">
                 <h1>Pesquisas disponíveis</h1>
-                <div className="card">
-                    <img src="./Researchs/img/worldverde.jpg">
-                    </img>
-                <div className="info">
-                    <h3 className="desc">Descrição qualquer</h3>
+            <div className="area-cards">
+            <div class="card" style={{width: "18rem"}} >
+                <img class="card-img-top" src={ImagemWorld} alt="Imagem de capa do card"/>
+                <div class="card-body">
+                    <h5 class="card-title">Pesquisa X</h5>
+                    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                    <a href="#" class="btn btn-primary">Responder</a>
                 </div>
-                </div>
+            </div>
+            </div> 
             </div>
         </div>
     )
