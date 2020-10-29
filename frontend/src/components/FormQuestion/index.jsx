0145e40d6ff70598d4f75a36783a7ca8afbe9ef5
Ana204 <60896884+Ana204@users.noreply.github.com>
2020-10-29 03:14:20 -0300

    stylizing the registration page, in the middle of the process

--- frontend/src/components/FormQuestion/index.jsx
+++ frontend/src/components/FormQuestion/index.jsx
@@ -19,19 +19,19 @@ function FormQuestion() {
     <form>
   
         <div class="titulo--pesquisa">
-          <label for=""> <h4>Título da pesquisa</h4> </label>  
+          <label for="">Título da pesquisa </label>  
            <input type="text" className="titulo" id="" placeholder="Ex:" onChange={e => setTitulo(e.target.value)} />
          </div>
       
 
       <div class="adicionar--pergunta">
-        <label for=""> <h4>Adicionar pergunta</h4></label> <br/> 
+        <label for="">Adicionar pergunta</label> <br/> 
         <input type="text" className="titulo" id="" placeholder="Ex:" onChange={e => setQuesta(e.target.value)} />
       </div>
 
       <fieldset class="form-group">
         <div class="respostas">
-          <legend class="col-form-label col-sm-2 pt-0"> <h4> Respostas </h4> </legend>
+          <legend class="col-form-label col-sm-2 pt-0"> Respostas  </legend>
           <div class="col-sm-10">
             <div class="valores">
             <label class="" for="gridRadios1">
@@ -58,23 +58,23 @@ function FormQuestion() {
               </label>
             </div>
           </div>
-
         </div>
       </fieldset>
-
+      <div className="respota--correta"> 
       <form class="form-inline">
-        <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Preference</label>
-        <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
-          <option selected>Choose...</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
+        <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Resposta-Correta</label>
+        <select class="selecao" id="inlineFormCustomSelectPref">
+          <option selected> A</option>
+          <option value="1">B</option>
+          <option value="2">C</option>
+          <option value="3">D</option>
         </select>
       </form>
+      </div>
 
       <div class="form-group">
-        <label for="exampleFormControlTextarea1">Feedback</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" onChange={e => setFeedback(e.target.value)}></textarea>
+        <label className="feedback" for="exampleFormControlTextarea1">Feedback</label>
+        <textarea class=" area--texto" id="exampleFormControlTextarea1" rows="3" onChange={e => setFeedback(e.target.value)}></textarea>
         <p>{feedback}</p>
       </div>
     </form>
