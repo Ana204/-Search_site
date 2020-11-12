9850bf5b29af97719a5a199cdae7f7bc0bf0422d
Denison Portela <denisondealmeidaprtela@hotmail.com>
2020-11-12 14:21:26 -0300

    OUT THE BACKGROUND COLOR WHITE IN REGISTER

--- frontend/src/Layouts/register/register.js
+++ frontend/src/Layouts/register/register.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 const Register =()=>{
     return(
       <div className="gradiente">
-         <div className="container">
+         <div className="container_register">
             <h2> Cadastrar nova pesquisa </h2> 
             <p></p>
             <div>
